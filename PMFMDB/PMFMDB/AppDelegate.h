79e8895d9aa25a8c39b41027ab88aeafdf76be58
王素燕 <wsyxyxs@126.com>
2015-12-01 08:08:27 +0800

    init project

---
+++ PMFMDB/PMFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PMFMDB
+//
+//  Created by wsy on 15/12/1.
+//  Copyright © 2015年 WSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PMFMDB/PMFMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PMFMDB
+//
+//  Created by wsy on 15/12/1.
+//  Copyright © 2015年 WSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
